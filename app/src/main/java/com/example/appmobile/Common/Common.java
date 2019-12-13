fade58e554da8197614389f17de540359bb714e9
CLEUET <cleuet@et.esiea.fr>
2019-12-13 13:23:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appmobile/Common/Common.java
@@ -0,0 +1,11 @@
+package com.example.appmobile.Common;
+
+import com.example.appmobile.Model.Pokemon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static List<Pokemon> commonPokemonList=new ArrayList<>();
+
+}
